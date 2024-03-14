c3645676cf638a63736b8a67616583c01e301017
animesh65432 <kirandutta234@gmail.com>
2024-03-14 14:17:28 +0530

    Making Nested Routes

---
+++ src/pages/about/Devolper.js
@@ -0,0 +1,9 @@
+const Devolper = () => {
+  return (
+    <>
+      <p>Hello My Name is Animesh Dutta</p>
+    </>
+  );
+};
+
+export default Devolper;
---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+const about = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the About Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default about;
